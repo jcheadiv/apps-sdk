fc0bb3dd759ae3355a953f000db2b96bf2747959
Thomas Rampelberg <thomas@saunter.org>
2010-08-06 14:59:31 -0700

    New client with the MSM in it

--- build.bash
+++ build.bash
@@ -6,4 +6,3 @@ python setup.py py2exe
 mv dist/apps-sdk.exe dist/apps.exe
 python lib.py
 PYTHONPATH=dist/ easy_install-2.6 -d dist -zUax ../dist/*.egg
-find /c/ -name "msvcr90.dll" | xargs -iFNAME cp "FNAME" dist/

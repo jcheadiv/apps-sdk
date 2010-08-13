f1d089f59bacd9f139ea5a0ec93a7aa2c72a44b7
Thomas Rampelberg <thomas@bittorrent.com>
2010-08-13 13:53:59 -0700

    Renamed and forgot to change the require

--- loader/apps-sdk.py
+++ loader/apps-sdk.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(sys.executable))
 import pkg_resources
-pkg_resources.require('apps')
+pkg_resources.require('apps-sdk')
 
 import apps.vanguard
 

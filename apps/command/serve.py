b1bc58340d5e0f29b8dc24edcfc8753df8b0bce9
Thomas Rampelberg <thomas@bittorrent.com>
2010-08-23 12:21:53 -0700

    Making sure caching doesn't happen

--- apps/command/serve.py
+++ apps/command/serve.py
@@ -79,6 +79,7 @@ class GriffinRequests(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.send_header("Content-type", ctype)
         fs = os.fstat(f.fileno())
         self.send_header("Content-Length", str(fs[6]))
+        self.send_header('Cache-Control', 'no-cache')
         self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
         self.end_headers()
         return f

789ea704e5e6c92f658f0154e5340701bfa40896
Bryan Kaplan <bkaplan@bittorrent.com>
2010-09-02 16:18:39 -0700

    Fixes for win32 installer.

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ rm -rf dist
 python setup.py py2exe
 mv dist/apps-sdk.exe dist/apps.exe
 python lib.py
-PYTHONPATH=dist easy_install-2.6 -d dist -ZUax ../dist/*.egg
+PYTHONPATH=dist easy_install-2.6 -d dist -zUax ../dist/*.egg
+cp -r "$VS90COMNTOOLS/../../VC/redist/x86/Microsoft.VC90.CRT/" dist
 popd

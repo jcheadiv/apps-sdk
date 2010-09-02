018dbe5af23fd7715ae094d18b0475befe4e57fe
Bryan Kaplan <bkaplan@bittorrent.com>
2010-09-02 12:48:00 -0700

    Changing build.sh to make easy_install not install as a zipfile, no matter what.

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ rm -rf dist
 python setup.py py2exe
 mv dist/apps-sdk.exe dist/apps.exe
 python lib.py
-PYTHONPATH=dist/ easy_install-2.6 -d dist -zUax ../dist/*.egg
+PYTHONPATH=dist easy_install-2.6 -d dist -ZUax ../dist/*.egg
 popd

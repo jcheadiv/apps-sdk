34259c5123dc35482dd893f0507d5616641dc069
Bryan Kaplan <bkaplan@bittorrent.com>
2010-09-02 12:31:45 -0700

    Ensuring build.sh executes in its home directory.

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+pushd $(dirname $0)
 python setup.py develop
 python setup.py bdist_egg
-cd loader/
-rm -rf dist/
+cd loader
+rm -rf dist
 python setup.py py2exe
 mv dist/apps-sdk.exe dist/apps.exe
 python lib.py
 PYTHONPATH=dist/ easy_install-2.6 -d dist -zUax ../dist/*.egg
+popd

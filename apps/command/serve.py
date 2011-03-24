7fe394f0c9333a5e3ff357ac744efdfe4a349fe0
Thomas Rampelberg <thomas@bittorrent.com>
2011-03-24 12:33:07 -0700

    Now with working get/post and fully implemented cookies

--- apps/command/serve.py
+++ apps/command/serve.py
@@ -2,18 +2,50 @@
 # Copyright (c) 2010 BitTorrent Inc.
 #
 
+import cookielib
+import functools
 import logging
 import mimetypes
 import os
 import pkg_resources
 import posixpath
+import time
+import tornado.httpclient
 import tornado.httpserver
 import tornado.options
 from tornado.options import define, options
+import tornado.simple_httpclient
 import tornado.web
 
 import apps.command.base
 import apps.vanguard
+
+class HTTPRequest(tornado.httpclient.HTTPRequest):
+
+    # This isn't hard coded by default ...
+    max_redirects = 5
+
+    def get_full_url(self):
+        return self.url
+
+    def is_unverifiable(self):
+        return not HTTPRequest.max_redirects == self.max_redirects
+
+    def has_header(self, header):
+        return header in self.headers
+
+    def add_unredirected_header(self, header, value):
+        self.headers.add(header, value)
+
+class HTTPResponse(tornado.httpclient.HTTPResponse):
+
+    def info(self):
+        return self
+
+    def getheaders(self, header):
+        return self.headers.get_list(header)
+
+tornado.simple_httpclient.HTTPResponse = HTTPResponse
 
 class FileHandler(tornado.web.RequestHandler):
 
@@ -25,6 +57,8 @@ class FileHandler(tornado.web.RequestHandler):
         return pth
 
     def get(self):
+        if self.request.path == '/':
+            self.request.path = '/index.html'
         fs_pth = self.build_path()
         if not os.path.exists(fs_pth):
             raise tornado.web.HTTPError(404)
@@ -53,6 +87,51 @@ class FileHandler(tornado.web.RequestHandler):
         '.h': 'text/plain',
         })
 
+class ProxyHandler(tornado.web.RequestHandler):
+
+    remove = {
+        'response': [ 'Transfer-Encoding', 'Status', 'Content-Encoding',
+                      'Content-Location', 'Connection', 'Host' ],
+        'request': [ 'Host', 'Cookie', 'X-Location', ],
+        }
+
+    def __init__(self, *args, **kwargs):
+        tornado.web.RequestHandler.__init__(self, *args, **kwargs)
+        self.cookie = self.application.settings.get('cookie')
+
+    def alter_headers(self, headers, direction, set_method):
+        for h,v in headers.iteritems():
+            if h in self.remove[direction]: continue
+            set_method(h, v)
+
+    @tornado.web.asynchronous
+    def get(self):
+        loc = self.request.headers.get('x-location', None)
+        if not loc:
+            raise tornado.web.HTTPError(404)
+
+        logging.info('PROXY ' + loc)
+        client = tornado.simple_httpclient.SimpleAsyncHTTPClient()
+        req = HTTPRequest(loc, method=self.request.method)
+        self.alter_headers(self.request.headers, 'request', req.headers.add)
+
+        if self.request.body:
+            req.body = self.request.body
+
+        self.cookie.add_cookie_header(req)
+        client.fetch(req, functools.partial(self.handle_response, req))
+
+    def handle_response(self, req, resp):
+        self.cookie.extract_cookies(resp, req)
+        self.cookie.save()
+        self.alter_headers(resp.headers, 'response', self.set_header)
+
+        self.set_status(resp.code)
+        self.write(resp.body)
+        self.finish()
+
+    post = get
+
 class serve(apps.command.base.Command):
 
     help = 'Run a development server to debug the project.'
@@ -64,6 +143,19 @@ class serve(apps.command.base.Command):
         define('debug', default=False, type=bool,
                help='Start the server in debug mode')
         define('root', default=self.project.path)
+        # Cookies get saved in the dist/ directory (as it never gets packaged
+        # up).
+        define('cookie_path', default=os.path.join(self.project.path, 'dist',
+                                                   'cookies.txt'))
+
+    def setup_cookie(self):
+        cookie = cookielib.MozillaCookieJar(
+            filename=options.cookie_path)
+        if not os.path.exists(options.cookie_path):
+            cookie.save()
+
+        cookie.load()
+        return cookie
 
     def run(self):
         logging.info('\tStarting server. Access it at http://localhost:%s/' %
@@ -78,8 +170,10 @@ class serve(apps.command.base.Command):
         logging.getLogger().setLevel(logging.INFO)
 
         application = tornado.web.Application([
+                (r"/proxy", ProxyHandler),
                 (r"/.*", FileHandler),
-                ], **{ "debug": options.debug })
+                ], **{ "debug": options.debug,
+                       "cookie": self.setup_cookie() })
         http_server = tornado.httpserver.HTTPServer(application)
         http_server.listen(int(self.options['port']))
         tornado.ioloop.IOLoop.instance().start()

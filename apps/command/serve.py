6ff76d668344245eac010d0a8f85bc4d31091c86
Thomas Rampelberg <thomas@bittorrent.com>
2011-03-29 11:12:07 -0700

    A couple changes to make this all work again

--- apps/command/serve.py
+++ apps/command/serve.py
@@ -170,7 +170,7 @@ class ProxyHandler(tornado.web.RequestHandler):
         if self.request.body:
             req.body = self.request.body
 
-        if req.body and not 'content-type' in headers:
+        if req.body and not 'Content-Type' in req.headers:
             req.headers.add('Content-Type', 'application/x-www-form-urlencoded')
 
         self.cookie.add_cookie_header(req)

5ff4c927f4d57c6415afa3ae1b1bb3e3cc3779a2
Skye <skye@bittorrent.com>
2011-03-29 17:11:09 -0700

    Fixes for gettext command

--- apps/command/gettxt.py
+++ apps/command/gettxt.py
@@ -1,4 +1,5 @@
 import glob
+import mako.template
 import os
 import pkg_resources
 import re
@@ -16,35 +17,33 @@ class gettxt(apps.command.base.Command):
     ]
     keyword = "gettext"
 
+    def messages(self, path, reg, remove):
+      for infile in glob.glob(path):
+        input = open(infile, "rb").read()
+        for m in re.finditer(reg, input):
+          start = m.start()
+          lineno = input.count('\n', 0, start) + 1
+          word = m.group(0).strip(remove)
+          print word
+          print len(re.sub(r'\s', '', word.strip()))
+          if len(word) and len(re.sub(r'\s', '', word.strip())):
+            if word in self.strings.keys():
+              self.strings[word].append("#: %s:%s" % (infile, lineno))
+            else: 
+              self.strings[word] = ["#: %s:%s" % (infile, lineno)]
+
     def run(self): 
       if self.options.get('keyword', False):
         self.keyword = self.options['keyword']
       kwreg = re.compile(r"(?<="+self.keyword+"\().*(?=\))", re.M)
-      template = open(pkg_resources.resource_filename(
-                'apps.data', self.template), 'rb').read()
+           
+      self.messages(os.path.join(self.project.path, "html/", "*.html"), self.regex, "")   
+      self.messages(os.path.join(self.project.path, "lib/", "*.js"), kwreg, '\'"')    
+      
+      template = mako.template.Template(
+                filename=pkg_resources.resource_filename(
+                'apps.data', self.template), cache_enabled=False)
       index = open(os.path.join(self.project.path, 'messages.po'), 'wb')
-      index.write(template+"\n\n")
-      
-      def messages(path, reg, remove):
-        for infile in glob.glob(path):
-          print infile
-          input = open(infile, "rb").read()
-          for m in re.finditer(reg, input):
-            start = m.start()
-            lineno = input.count('\n', 0, start) + 1
-            word = m.group(0).strip().strip(remove)
-            if len(word) and word in self.strings.keys():
-              self.strings[word].append("#: %s:%s" % (infile, lineno))
-            else: 
-              self.strings[word] = ["#: %s:%s" % (infile, lineno)]
-      
-      messages(os.path.join(self.project.path, "html/", "*.html"), self.regex, "")   
-      messages(os.path.join(self.project.path, "lib/", "*.js"), kwreg, '\'"')    
-      
-      for s in self.strings.keys():
-        for o in self.strings[s]:
-          index.write(o+"\n")
-        index.write("msgid \"%s\"\n" % (s))
-        index.write("msgtr \"\"\n\n")
+      index.write(template.render(strings=self.strings))
       
       index.close()
